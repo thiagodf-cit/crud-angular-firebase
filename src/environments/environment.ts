2b5d0fc662e3c4342fa3b7fea67ef12cfb635b06
thiagodf <thiagodf@ciandt.com>
2020-06-02 13:18:39 -0300

    include firebase domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCqnQcKmS4nvA9hjvkP8Q1xJemqVHB_Sis",
+    authDomain: "crud-angular-firebase-eee8f.firebaseapp.com",
+    databaseURL: "https://crud-angular-firebase-eee8f.firebaseio.com",
+    projectId: "crud-angular-firebase-eee8f",
+    storageBucket: "crud-angular-firebase-eee8f.appspot.com",
+    messagingSenderId: "236911506356",
+    appId: "1:236911506356:web:d9a2ee3564f9e0a642b8e1",
+    measurementId: "G-BHR6T1QQPT"
+  }
 };
 
 /*
