ebf4d1d0a198fc27c65c7b51b4c3bbfcfc781e0e
thiagodf <thiagodf@ciandt.com>
2020-06-02 18:37:25 -0300

    final list

--- src/app/contacts/list/list.component.ts
+++ src/app/contacts/list/list.component.ts
@@ -1,4 +1,9 @@
+import { Contact } from './../shared/contact';
 import { Component, OnInit } from '@angular/core';
+import { ContactService } from './../service/contact.service';
+import { Observable } from 'rxjs';
+import { ContactDataService } from './../shared/contact-data.service';
+
 
 @Component({
   selector: 'app-list',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  contacts: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private contactService: ContactService,
+    private contactDataService: ContactDataService
+  ) { }
 
   ngOnInit() {
+    this.contacts = this.contactService.getAll();
   }
 
+  delete(key: string) {
+    this.contactService.delete(key);
+  }
+
+  edit(contact: Contact, key: string) {
+    this.contactDataService.changeContact(contact, key);
+  }
 }
