8e43880c14bdeb8f8b6f909e000114398041f5ea
thiagodf <thiagodf@ciandt.com>
2020-06-02 18:37:13 -0300

    final edit

--- src/app/contacts/edit/edit.component.ts
+++ src/app/contacts/edit/edit.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from './../shared/contact';
+import { ContactService } from '../service/contact.service';
+import { ContactDataService } from '../shared/contact-data.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
-export class EditComponent implements OnInit {
 
-  constructor() { }
+export class EditComponent implements OnInit {
+  contact: Contact;
+  key = '';
+
+  constructor(
+    private contactService: ContactService,
+    private contactDataService: ContactDataService
+  ) { }
 
   ngOnInit() {
+    this.contact = new Contact();
+    this.contactDataService.currentContact.subscribe(data =>{
+      if (data.contact && data.key) {
+        this.contact = new Contact();
+        this.contact.name = data.contact.name;
+        this.contact.email = data.contact.email;
+        this.contact.phone = data.contact.phone;
+        this.key = data.key;
+      }
+    });
   }
 
+  onSubmit() {
+    if (this.key) {
+      this.contactService.update(this.contact, this.key);
+    } else {
+      this.contactService.insert(this.contact);
+    }
+    this.contact = new Contact();
+  }
 }
