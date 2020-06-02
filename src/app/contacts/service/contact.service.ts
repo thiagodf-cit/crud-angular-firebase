6b8f8ef8da46fa4efc5e8ddbbfc52b15282fc26d
thiagodf <thiagodf@ciandt.com>
2020-06-02 14:03:18 -0300

    create insert, update, getAll and delete

--- src/app/contacts/service/contact.service.ts
+++ src/app/contacts/service/contact.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { Contact } from '../shared/contact';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private fireStore: AngularFireDatabase) { }
+
+  insert(contact: Contact){
+    this.fireStore.list('contact').push(contact)
+      .then((result: any) => {
+        console.log(result.key);
+      });
+  }
+
+  update(contact: Contact, key: string){
+    this.fireStore.list('contact').update(key, contact)
+    .catch((error: any) => {
+      console.log(error);
+    });
+  }
+
+  getAll(){
+    return this.fireStore.list('contact')
+      .snapshotChanges()
+      .pipe(
+        map(changes => {
+          return changes.map(c => ({ key: c.payload.key, ...c.payload.val }));
+        })
+      );
+  }
+
+  delete(key: string){
+    this.fireStore.object(`contact/$(key)`).remove();
+  }
 }
